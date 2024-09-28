1e1de43474269231824830d34adc1d3dfdc25eb5
Daniel Sarmiento <jsarmiento@axede.com.co>
2024-09-27 23:19:34 -0500

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This try to use yolov11 " << std::endl;
+    
+    return 0;
+}
